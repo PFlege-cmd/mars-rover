784b3f37594c682018cb8b1eab1b86571693f5b4
patrick.flege@rabobank.nl <Patrick.Flege@rabobank.nl>
2023-03-14 13:22:10 +0100

    First commit

---
+++ mars-rover/src/test/java/org/example/MarsRoverTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MarsRoverTest {
+
+    @Test
+    void testTrue(){
+        Assertions.assertEquals(1,1);
+    }
+}
